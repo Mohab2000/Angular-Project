0886dbcb42de256a952518462bd412a0f8cd3829
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-03 11:08:33 +0200

    toolbar done

---
+++ src/app/contactmanager/contact-manager-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-manager-app',
+  template: ` <app-sidenav> </app-sidenav> `,
+  styles: [],
+})
+export class ContactManagerAppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
