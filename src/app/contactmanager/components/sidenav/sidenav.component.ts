29b830bf14a13444a6d71d8f4ef824d3f256f4bd
Mohab2000 <mohabahmed20005@gmail.com>
2022-11-06 23:59:45 +0200

    Last Commit

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -17,6 +17,8 @@ export class SidenavComponent implements OnInit {
   public isScreenSmall: boolean;
 
   users: Observable<User[]>;
+  isDarkTheme: boolean = false;
+  dir: string = 'ltr';
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -25,6 +27,14 @@ export class SidenavComponent implements OnInit {
   ) {}
 
   @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
+
+  toggleDir() {
+    this.dir = this.dir == 'ltr' ? 'rtl' : 'ltr';
+  }
 
   ngOnInit(): void {
     this.breakpointObserver
